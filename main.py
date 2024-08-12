4d265a1c102b32a1bd553f8d7343b975e93fbcd7
Sara Fontes <sfontes94@gmail.com>
2024-08-12 12:26:38 +0100

    updated readme info

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from img.ascii import greetings_coffee
+from img.ascii import greetings_coffee, coffee
 from src.data import data, menu, cash
 
 
 def new_order():
     print(greetings_coffee) 
-    print("Hello there ! Welcome to our Virtual Coffee Machine.")
+    print("Hello there! Welcome to our Virtual Coffee Machine.")
     while True:     
-        ask = input("What would you like? Type Espresso, Latte, Cappucino (or 'E' for exit): ").lower()
+        ask = input("What would you like? Type Espresso, Latte, Cappuccino (or 'E' for exit): ").lower()
         if ask == "espresso":
             return "espresso"
         elif ask == "latte":
             return "latte"
-        elif ask == "cappucino":
-            return "cappucino"
+        elif ask == "cappuccino":
+            return "cappuccino"
         elif ask == "off":
             return "off"
         elif ask == "e":
@@ -23,15 +23,12 @@ def new_order():
             print("Invalid Request.")
 
 
-
 def report():
     water = data[0]["ingredient"] + ": " + str(data[0]["amount"]) + data[0]["measure"]
     milk = data[1]["ingredient"] + ": " + str(data[1]["amount"]) + data[1]["measure"]
     coffee = data[2]["ingredient"] + ": " + str(data[2]["amount"]) + data[2]["measure"]
     print(water + "\n" + milk + "\n" + coffee)
     print(f"Money: ${cash['money']:.2f}")
-    
-
 
 
 def is_resource_sufficient(order_ingredients):
@@ -52,7 +49,7 @@ def process_coins():
     total = 0
     total += int(input("How many quarters?: ")) * 0.25
     total += int(input("How many dimes?: ")) * 0.1
-    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many nickels?: ")) * 0.05
     total += int(input("How many pennies?: ")) * 0.01
     return total
 
@@ -69,29 +66,26 @@ def main():
 
     while on:
         order = new_order()
-        # drink = menu[order]
-        total_cash = cash["money"]
         if order == "off":
-            print("bye bye!")
+            print("Bye bye!")
             on = False
-        if order == "exit":
+        elif order == "exit":
             print("See you soon!")
             on = False    
         elif order == "report":
             report()
         else:
-                if is_resource_sufficient(menu[order]["ingredients"]):
-                    payment = process_coins()
-                    if payment > int(menu[order]["cost"]):
-                        update_ingredients(menu[order]["ingredients"])
-                        initial_cash = cash["money"]
-                        change = round(payment - menu[order]["cost"], 2)
-                        total_cash = initial_cash + payment - change
-                        print(f"Here is ${change:.2f} in change.")
-                        print(f"Enjoy your {order}!")
-                        print(f"test  {total_cash:.2f}")
+            if is_resource_sufficient(menu[order]["ingredients"]):
+                payment = process_coins()
+                if payment >= menu[order]["cost"]:
+                    change = round(payment - menu[order]["cost"], 2)
+                    cash["money"] += menu[order]["cost"]
+                    update_ingredients(menu[order]["ingredients"])
+                    print(coffee)
+                    print(f"Here is ${change:.2f} in change.")
+                    print(f"Enjoy your {order}!\n")
                 else:
-                        print("Sorry, that's not enough money. Money refunded.")
+                    print("Sorry, that's not enough money. Money refunded.")
 
 
 main()
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Virtual Coffee Machine Project
+
+Welcome to the Virtual Coffee Machine Project! This project built with Python simulates a coffee machine, allowing you to select and brew different types of coffee virtually.
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [Features](#features)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [Example Output](#example-output)
+
+## Introduction
+
+The Virtual Coffee Machine project aims to create a simple yet functional coffee machine simulation. It allows users to select from a variety of coffee options, process their choice, and get a virtual "brewed" coffee as output.
+
+## Features
+
+- Multiple coffee options to choose from
+- Simulation of brewing process
+- Easy-to-use interface
+- Payment options
+- Checks for ingredients stock in the machine
+- Two secret options for maintainers and coworkers:
+    - "off" to turn off the machine
+    - "report" to show cashflow and ingredients stock
+
+
+## Example Output
+Here's an example of what the output looks like when you brew a coffee:
+
+<img src="img/coffee_print1.png" alt="Coffee Output" max-width="300">
+
+<img src="img/coffee_print2.png" alt="Coffee Output" max-width="300">
+
+
+## Installation
+
+To get started with the Virtual Coffee Machine, follow these steps:
+
+1. Clone the repository.
+2. To run the Virtual Coffee Machine, you can use the provided batch file for easier execution:
+
+
+## Example Output
+
+Execute the program by running the batch file:
+
+`Play_Virtual_Coffee_Machine.bat`
+
+This will launch the coffee machine simulation, and you can start brewing your virtual coffee!
+
